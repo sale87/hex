a2cedb5e20e5153f1405343c22ba1f4ed1776469
Sasa Ranisavljevic <sasa.ranisavljevic@gmail.com>
2014-10-28 23:29:39 +0100

    added authentication failure message

--- client/auth/sign_in_controller.js
+++ client/auth/sign_in_controller.js
@@ -3,6 +3,8 @@
   var hex = angular.module('hex');
 
   hex.controller('SignInController', ['$scope', function ($scope) {
-
+    $scope.$on('auth:login-error', function (ev, reason) {
+      $scope.error = reason.errors[0];
+    });
   }]);
 }());
--- spec/features/users/authentication_spec.rb
+++ spec/features/users/authentication_spec.rb
@@ -17,5 +17,10 @@ feature 'Authentication', js: true do
       @login_page.sign_in(@user.email, @user.password)
       expect(page).to have_content('Welcome to Hex!')
     end
+
+  scenario 'with invalid credentials' do
+      @login_page.sign_in('notexisting@email.com', 'blabla')
+      expect(page).to have_content('Invalid login credentials. Please try again.')
+    end
   end
 end
